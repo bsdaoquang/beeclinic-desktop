e56bf655ef7194f6436fc969d6846e5692bd8a20
bsdaoquang <bsdaoquang@gmail.com>
2025-08-18 10:29:04 +0700

    add icons

--- main/main.js
+++ main/main.js
@@ -43,6 +43,7 @@ app.whenReady().then(() => {
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
 });
+
 ipcMain.handle('add-patient', (event, patient) => {
 	return new Promise((resolve, reject) => {
 		const query = `
--- renderer/src/components/FooterComponent.tsx
+++ renderer/src/components/FooterComponent.tsx
@@ -19,35 +19,12 @@ const FooterComponent = () => {
 					</div>
 					<div className='col text-end'>
 						<Typography.Text>
-							Ver {require('../../../package.json').version} -{' '}
-							<a href=''>All rights reserved</a>
+							Ver - <a href=''>All rights reserved</a>
 						</Typography.Text>
 					</div>
 				</div>
-				{/* <Row>
-					<Col span={12} style={{ textAlign: 'left' }}>
-						<Typography.Paragraph style={{ margin: 0 }}>
-							<a href='https://yhocso.com'>Y Học Số</a> © 2025
-						</Typography.Paragraph>
-					</Col>
-					<Col span={12} style={{ textAlign: 'right' }}>
-						<Typography.Paragraph style={{ margin: 0 }}>
-							Ver 1.0.0 - <a href=''>All rights reserved</a>
-						</Typography.Paragraph>
-					</Col>
-				</Row> */}
 			</div>
 		</Affix>
-		// <div
-		// 	style={{
-		// 		padding: '10px',
-		// 		backgroundColor: '#e0e0e0',
-		// 		height: 20,
-		// 	}}>
-		// 	<Typography.Paragraph>
-		// 		FooterComponent - © 2023 Your Company Name
-		// 	</Typography.Paragraph>
-		// </div>
 	);
 };
 
