f1d328f157021db52db3d7e5e62f0e2becb1c8f2
bsdaoquang <bsdaoquang@gmail.com>
2025-09-07 23:10:47 +0700

    fix sync data

--- renderer/src/routers/Routers.tsx
+++ renderer/src/routers/Routers.tsx
@@ -1,11 +1,13 @@
 /** @format */
 
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import handleAPI from '../apis/handleAPI';
 import FooterComponent from '../components/FooterComponent';
 import HeaderComponent from '../components/HeaderComponent';
 import RouterComponent from './RouterComponent';
-import { useEffect } from 'react';
-import handleAPI from '../apis/handleAPI';
+import { addClinic } from '../store/reducers/clinic-reducer';
 
 const Routers = () => {
 	// khi app được mở, nghĩa là bảng phòng khám đã được tạo
@@ -17,13 +19,14 @@ const Routers = () => {
 	}, []);
 
 	const isOnline = navigator.onLine;
+	const dispatch = useDispatch();
 
 	const handleSyncClinicData = async () => {
 		try {
 			const machineId = await window.beeclinicAPI.getMachineId();
 			const { version } = await window.beeclinicAPI.getVersion();
 
-			const data = {
+			let data: any = {
 				MachineId: machineId,
 				AppVersion: version,
 				ActivationKey: '',
@@ -41,12 +44,50 @@ const Routers = () => {
 							_id: resServer._id,
 							...data,
 						});
+
+						data._id = resServer._id;
 					}
 				}
+
+				data.id = id;
 			} else {
-				// Đã có phòng khám
-				console.log(res);
+				// đã có phòng khám
+				const clinic = res[0];
+				// nếu chưa có _id là app trước đó chưa có mạng, hoặc app đã được cài trước khi làm phần này
+				if (isOnline) {
+					if (!clinic._id) {
+						const resServer = await handleAPI('/clinic', clinic, 'post');
+						if (resServer && resServer._id) {
+							// cập nhật _id từ server về phòng khám
+							await window.beeclinicAPI.updateClinicById(clinic.id, {
+								_id: resServer._id,
+							});
+						}
+
+						data = { ...clinic, _id: resServer._id };
+					} else {
+						// đã có _id, đồng bộ dữ liệu
+						// update lên server, không update ActivationKey
+						const { _id, ActivationKey, ...rest } = clinic;
+						await handleAPI(`/clinic/${_id}`, rest, 'put');
+
+						// cập nhật activation key về tránh hack
+						const res = await handleAPI(`/clinic/${clinic._id}`);
+						if (res && res._id) {
+							// cập nhật dữ liệu mới nhất từ server về phòng khám
+							await window.beeclinicAPI.updateClinicById(clinic.id, {
+								ActivationKey: res.ActivationKey,
+							});
+
+							data = { ...clinic, ActivationKey: res.ActivationKey };
+						}
+					}
+				} else {
+					data = { ...clinic };
+				}
 			}
+
+			dispatch(addClinic(data));
 		} catch (error) {
 			console.log(error);
 		}
